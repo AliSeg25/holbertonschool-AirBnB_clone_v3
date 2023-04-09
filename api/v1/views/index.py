1eb645f9dce5be8e9700129d3c0ce44231504587
Your Name <you@example.com>
2023-04-09 18:17:54 +0200

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,9 +6,19 @@ app
 from flask import jsonify
 import sys
 from api.v1.views import app_views
+from models import storage
+sys.path.append("/home/ali/holbertonschool-AirBnB_clone_v3")
 sys.path.append("/home/ali/holbertonschool-AirBnB_clone_v3")
 
 
 @app_views.route("/status", methods=["GET"])
 def get_status():
     return jsonify({"status": "OK"})
+
+
+@app_views.route("/api/v1/stats", methods=["GET"])
+def get_stats():
+    stats = {}
+    for cls in storage.classes().values():
+        stats[cls.__name__.lower() + "s"] = storage.count(cls)
+    return jsonify(stats)
