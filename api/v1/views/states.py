85c46e9a5f1022753262535c9a3123c05c033b1e
Your Name <you@example.com>
2023-04-09 20:01:02 +0200

    7. State

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -43,7 +43,7 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
 @app_views.route('/states/', methods=['POST'], strict_slashes=False)
