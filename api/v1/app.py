c9ac61952cef75924e57051a1244188a48b88810
Your Name <you@example.com>
2023-04-09 16:00:02 +0200

    4. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,40 +1,19 @@
 #!/usr/bin/python3
+"""
+app
+"""
+
 from flask import Flask
 import sys
 from models import storage
 from api.v1.views import app_views
 import os
 sys.path.append("/home/ali/holbertonschool-AirBnB_clone_v3")
-"""
-4. Status of your API
-mandatory
-It’s time to start your API!
 
-Your first endpoint (route) will be
-to return the status of your API:
-"""
 
-# create a variable app, instance of Flask
 app = Flask(__name__)
 
-
-"""
-L'enregistrement du blueprint app_views avec l'instance
-Flask appsignifie que les routes définies dans ce
-blueprint seront disponibles pour l'application Flask
-"""
 app.register_blueprint(app_views)
-
-
-"""
-déclarer une méthode pour gérer @app.teardown_appcontext
-qui appelle storage.close()@app.teardown_appcontext est
-un décorateur de Flask qui indiquequ'une fonction doit
-être appelée à la fin de chaque requête HTTP.
-En utilisant cette déclaration, vous pouvez
-être sûr que la connexion
-avec le stockage sera toujours fermée correctement.
-"""
 
 
 @app.teardown_appcontext
