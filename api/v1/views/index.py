626b420287b9c64b8365b65a7230632de3ed9f38
Your Name <you@example.com>
2023-04-09 18:36:06 +0200

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -9,6 +9,12 @@ sys.path.append("/home/ali/holbertonschool-AirBnB_clone_v3")
 sys.path.append("/home/ali/holbertonschool-AirBnB_clone_v3")
 from api.v1.views import app_views
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status", methods=["GET"])
@@ -16,7 +22,7 @@ def get_status():
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+@app_views.route("/api/v1/stats", methods=["GET"])
 def get_stats():
     """Permet de recupere tout les class dans un dict et le nombre instance"""
     stats = {}
